93ddd3c3b5448f03cd5e055ff2255b815036ce7b
alexeytas <alexdali@gmail.com>
2020-11-08 02:45:56 +0400

    feat: update guess_num.py

--- guess_num.py
+++ guess_num.py
@@ -16,3 +16,33 @@
 
 if __name__ == '__main__':
     pass
+    import random
+    random_number = random.randint(0, 1_000_000)
+    print('''Привет! Давай поиграем в игру.
+        Угадай число между 0 и 1 000 000!
+    ''')
+
+    while True:
+        try:
+            input_of_user = input('Введи число между 0 и 1 000 000: ')
+            if (input_of_user == 'exit' or len(input_of_user) == 0):
+                print('До новых встреч!')
+                break
+            input_to_int = int(input_of_user)
+        except ValueError:
+            print('Вводить можно только числа! Попробуй еще раз.')
+            continue
+        else:
+            if (input_to_int < 0 or input_to_int > 1_000_000):
+                print('Вводить можно только числа между 0 и 1 000 000!\nПопробуй еще раз.')
+                continue
+            elif input_to_int > random_number:
+                print('Меньше')
+                continue
+            elif input_to_int < random_number:
+                print('Больше')
+                continue
+            elif input_to_int == random_number:
+                print('BINGO!!!')
+                break
+
