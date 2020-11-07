1d5d13542c86b011801b93bf047d73c103f53302
alexeytas <alexdali@gmail.com>
2020-11-08 02:02:45 +0400

    feat: update password.py

--- password.py
+++ password.py
@@ -8,3 +8,11 @@
 
 if __name__ == '__main__':
     pass
+
+    passwd = input('Введите пароль: ')
+    contains_digit = any(char.isdigit() for char in passwd)
+    contains_alpha = any(char.isalpha() for char in passwd)
+    if (len(passwd) >= 8 and contains_digit and contains_alpha):
+        print('Ваш пароль сложный')
+    else:
+        print('Ваш пароль простой')
